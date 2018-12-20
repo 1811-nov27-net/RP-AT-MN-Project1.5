227d9563abdebad0ba9034d7e25e328b46590513
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-12-19 19:05:15 -0600

    Models

---
+++ Project1-5_MVC_REST/Project1-5_MVC_REST/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project1_5_MVC_Rest.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        
+        public decimal Salary { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Role { get; set; }
+    }
+}
---
+++ Project1-5_MVC_REST/Project1-5_MVC_REST/Models/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project1_5_MVC_Rest.Models
+{
+    public partial class Reservation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public int RoomId { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        public decimal TotalCost { get; set; }
+        
+        public virtual Customer Customer { get; set; }
+        public virtual Room Room { get; set; }
+    }
+}
