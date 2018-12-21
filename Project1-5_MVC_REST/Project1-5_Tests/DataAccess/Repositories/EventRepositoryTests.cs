84a01d52644ad46fcc0b6ae756fbcb089ca5da75
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-12-21 12:52:56 -0600

    Sccafold Repositories Tests

---
+++ Project1-5_MVC_REST/Project1-5_Tests/DataAccess/Repositories/EventRepositoryTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Xunit;
+
+namespace Project1_5_Tests.DataAccess.Repositories
+{
+    public class EventRepositoryTests : IRepositoriesTest
+    {
+        [Fact]
+        public void CreateAddressesWorks()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void DeleteWithIdThatDoesntExistThrowsException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void DeleteWorks()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetAllWorks()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetByIdThatDoesntExistReturnsNull()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GetByIdWorks()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void UpdateWithNoIdShouldReturnException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void UpdateWithWorngIdShouldReturnException()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void UpdateWorks()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Project1-5_MVC_REST/Project1-5_Tests/DataAccess/Repositories/IRepositoriesTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1_5_Tests.DataAccess.Repositories
+{
+    interface IRepositoriesTest
+    {
+        void CreateAddressesWorks();
+        void UpdateWorks();
+        void UpdateWithWorngIdShouldReturnException();
+        void UpdateWithNoIdShouldReturnException();
+        void GetAllWorks();
+        void GetByIdWorks();
+        void GetByIdThatDoesntExistReturnsNull();
+        void DeleteWorks();
+        void DeleteWithIdThatDoesntExistThrowsException();
+    }
+}
