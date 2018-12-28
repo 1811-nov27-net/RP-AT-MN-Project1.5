0007b78975740bd9c4055b1330e3ad4036ac5a30
Rogerio Pereira <rogerio@colmeiatecnologia.com.br>
2018-12-28 09:16:11 -0600

    Removed unused methods from CheckoutController

--- Project1-5_MVC_REST/Project1-5_MVC_REST/Controllers/CheckoutController.cs
+++ Project1-5_MVC_REST/Project1-5_MVC_REST/Controllers/CheckoutController.cs
@@ -11,36 +11,11 @@ namespace Project1_5_MVC_REST.Controllers
     [ApiController]
     public class CheckoutController : ControllerBase
     {
-        // GET: api/Checkout
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
         // GET: api/Checkout/5
         [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "value";
-        }
-
-        // POST: api/Checkout
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/Checkout/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
         }
     }
 }
