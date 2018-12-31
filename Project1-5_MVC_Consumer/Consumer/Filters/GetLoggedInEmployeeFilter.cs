cd7ce04d9cd124eb92c0eab749adadd07c99ee61
Marty McFly <martinjnoyes12@gmail.com>
2018-12-31 17:32:31 -0600

    adding to revert changes arch design changes simplifiying

---
+++ Project1-5_MVC_Consumer/Consumer/Filters/GetLoggedInEmployeeFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Consumer.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Consumer.Filters
+{ 
+	public class GetLoggedInEmployeeFilter : IAsyncActionFilter
+	{
+		public async Task OnActionExecutionAsync(
+						ActionExecutingContext context,
+						ActionExecutionDelegate next)
+		{
+
+			var controller = context.Controller as AServiceController;
+			if (controller != null)
+			{
+				HttpRequestMessage request = controller.CreateRequestToService(HttpMethod.Get, "api/Employee/loggedinuser");
+				HttpResponseMessage response = await controller.Client.SendAsync(request);
+
+				if (!response.IsSuccessStatusCode)
+				{
+					controller.ViewBag.LoggedInUser = "";
+				}
+				controller.ViewBag.LoggedInUser = await response.Content.ReadAsStringAsync();
+				//puts logged in user into view bag
+			}
+					var resultContext = await next();
+		}
+	}
+}
---
+++ Project1-5_MVC_REST/Project1-5_MVC_REST/Controllers/EmployeeLoginController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project1_5_MVC_REST.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class EmployeeController : ControllerBase
+	{
+		private IEmployeeRepository Repository { get; set; }
+
+		public EmployeeController(IEmployeeRepository _respository)
+		{
+			Repository = _respository;
+		}
