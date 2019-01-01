75753c5ec1d1722874788dad23a1b563b3809597
axelt18 <tovar_axel@yahoo.com>
2019-01-01 15:03:57 -0600

    more tests have been finished

--- Project1-5_MVC_REST/Project1-5_Tests/DataAccess/Repositories/EventCustomerRepositoryTests.cs
+++ Project1-5_MVC_REST/Project1-5_Tests/DataAccess/Repositories/EventCustomerRepositoryTests.cs
@@ -18,35 +18,50 @@ namespace Project1_5_Tests.DataAccess.Repositories
         [Fact]
         public override async Task CreateWorksAsync()
         {
+            Customers customerSaved = null;
             Event EventSaved = null;
+            EventCustomer eventCustomerSaved = null;
 
-            // arrange (use the context directly - we assume that works)
             var options = new DbContextOptionsBuilder<Project15Context>()
-                .UseInMemoryDatabase("db_event_test_create").Options;
+                .UseInMemoryDatabase("db_eventcustomer_test_create").Options;
+
             using (var db = new Project15Context(options)) ;
 
             // act (for act, only use the repo, to test it)
             using (var db = new Project15Context(options))
             {
-                var repo = new EventRepository(db);
+                var repo = new EventCustomerRepository(db);
 
-                //Create Event
-                Event Event = new Event { Name = "Jazz Fest", Type = "Music", Cost = 40 };
-                EventSaved = await repo.CreateAsync(Event);
+                //Create customer
+                Customers customer = new Customers { Name = "Axel", Address1 = "111 Address" };      
                 await repo.SaveChangesAsync();
+
+                Event Event = new Event { Name = "Jazz Fest", Type = "Music", Cost = 40 };
+                await repo.SaveChangesAsync();
+
+                EventCustomer eventCustomer = new EventCustomer
+                {
+                    CustomerId = customer.Id,
+                    EventId = Event.Id,
+                    Paid = true
+                };
+                eventCustomerSaved = await repo.CreateAsync(eventCustomer);
+                await repo.SaveChangesAsync();
+                customerSaved = customer;
+                EventSaved = Event;
+
             }
 
             // assert (for assert, once again use the context directly for verify.)
             using (var db = new Project15Context(options))
             {
-                var repo = new EventRepository(db);
-                Event Event = Mapper.Map<Events, Event>(db.Events.First(m => m.Name == "Jazz Fest"));
+                var repo = new EventCustomerRepository(db);
+                EventCustomer eventCustomer = Mapper.Map<EventsCustomers, EventCustomer>(db.EventsCustomers.First(m => m.Paid == true));
 
-                Assert.NotEqual(0, Event.Id); // should get some generated ID
-                Assert.Equal(EventSaved.Id, Event.Id);
-                Assert.Equal("Jazz Fest", Event.Name);
-                Assert.Equal("Music", Event.Type);
-                Assert.Equal(40, Event.Cost);
+                Assert.Equal(customerSaved.Id, eventCustomer.CustomerId);
+                Assert.Equal(EventSaved.Id, eventCustomer.EventId);
+                Assert.True(eventCustomer.Paid);            
+                Assert.NotEqual(0, eventCustomer.Id);
             }
         }
 
@@ -54,68 +69,88 @@ namespace Project1_5_Tests.DataAccess.Repositories
         public override async Task DeleteWithIdThatDoesntExistThrowsExceptionAsync()
         {
             int id = 1000;
-
+            // arrange (use the context directly - we assume that works)
             var options = new DbContextOptionsBuilder<Project15Context>()
-                .UseInMemoryDatabase("db_Event_test_delete").Options;
-
-            // arrange (use the context directl - we assume that it works)
-            using (var db = new Project15Context(options)) ;
-
+                .UseInMemoryDatabase("db_eventcustomer_test_delete").Options;
+            using (var db = new Project15Context(options));
 
             // assert (for assert, once again use the context directly for verify.)
             using (var db = new Project15Context(options))
             {
-                var repo = new EventRepository(db);
+                var repo = new EventCustomerRepository(db);
 
-                Event Event = await repo.GetByIdAsync(id);
-
-                Assert.Null(Event);
+                EventCustomer eventCustomer = await repo.GetByIdAsync(id);
+              
+                Assert.Null(eventCustomer);
 
                 await Assert.ThrowsAsync<ArgumentException>(() => repo.DeleteAsync(id));
+
             }
         }
 
         [Fact]
         public override async Task DeleteWorksAsync()
         {
-            Event EventSaved = null;
             int id = 0;
+
+            Customer customerSaved = null;
+            Event EventSaved = null;
+            EventCustomer eventCustomerSaved = null;
 
             // arrange (use the context directly - we assume that works)
             var options = new DbContextOptionsBuilder<Project15Context>()
-                .UseInMemoryDatabase("db_Event_test_delete").Options;
+                .UseInMemoryDatabase("db_eventcustomer_test_delete_2").Options;
+
             using (var db = new Project15Context(options)) ;
 
             // act (for act, only use the repo, to test it)
             using (var db = new Project15Context(options))
             {
-                var repo = new EventRepository(db);
 
-                //Create Event
+                var customerRepo = new CustomerRepository(db);
+                var eventRepo = new EventRepository(db);
+                var repo = new EventCustomerRepository(db);
+
+                //Create customer
+                Customer customer = new Customer { Name = "Axel", Address1 = "111 Address" };
+                customer = await customerRepo.CreateAsync(customer);
+                await customerRepo.SaveChangesAsync();
+
                 Event Event = new Event { Name = "Jazz Fest", Type = "Music", Cost = 40 };
-                EventSaved = await repo.CreateAsync(Event);
+                Event = await eventRepo.CreateAsync(Event);
+                await eventRepo.SaveChangesAsync();
+
+                EventCustomer eventCustomer = new EventCustomer
+                {
+                    CustomerId = customer.Id,
+                    EventId = Event.Id,
+                    Paid = true
+                };
+
+                eventCustomerSaved = await repo.CreateAsync(eventCustomer);
                 await repo.SaveChangesAsync();
 
-                id = EventSaved.Id;
+                EventSaved = Event;
+                customerSaved = customer;
+
+                id = eventCustomerSaved.Id;
             }
 
             // assert (for assert, once again use the context directly for verify.)
             using (var db = new Project15Context(options))
             {
-                var repo = new EventRepository(db);
-                Event Event = await repo.GetByIdAsync(id);
+                var repo = new EventCustomerRepository(db);
+                EventCustomer eventCustomer = await repo.GetByIdAsync(id);
 
-                Assert.NotEqual(0, Event.Id); // should get some generated ID
-                Assert.Equal(EventSaved.Id, Event.Id);
-                Assert.Equal("Jazz Fest", Event.Name);
-                Assert.Equal("Music", Event.Type);
-                Assert.Equal(40, Event.Cost);
+                Assert.NotEqual(0, eventCustomer.Id);
+                Assert.Equal(eventCustomerSaved.Id, eventCustomer.Id);
+                Assert.True(eventCustomer.Paid);
 
                 await repo.DeleteAsync(id);
                 await repo.SaveChangesAsync();
-                Event = await repo.GetByIdAsync(id);
+                eventCustomer = await repo.GetByIdAsync(id);
 
-                Assert.Null(Event);
+                Assert.Null(eventCustomer);
             }
         }
 
--- Project1-5_MVC_REST/Project1-5_Tests/DataAccess/Repositories/EventRepositoryTests.cs
+++ Project1-5_MVC_REST/Project1-5_Tests/DataAccess/Repositories/EventRepositoryTests.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Project1_5_DataAccess;
+using Project1_5_DataAccess.Repositories;
+using Project1_5_Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -9,57 +16,325 @@ namespace Project1_5_Tests.DataAccess.Repositories
         public EventRepositoryTests() : base() { }
 
         [Fact]
-        public override Task CreateWorksAsync()
+        public override async Task CreateWorksAsync()
         {
-            throw new NotImplementedException();
+            Event EventSaved = null;
+
+            // arrange (use the context directly - we assume that works)
+            var options = new DbContextOptionsBuilder<Project15Context>()
+                .UseInMemoryDatabase("db_event_test_create").Options;
+            using (var db = new Project15Context(options)) ;
+
+            // act (for act, only use the repo, to test it)
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+
+                //Create Event
+                Event Event = new Event { Name = "Jazz Fest", Type = "Music", Cost = 40 };
+                EventSaved = await repo.CreateAsync(Event);
+                await repo.SaveChangesAsync();
+            }
+
+            // assert (for assert, once again use the context directly for verify.)
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+                Event Event = Mapper.Map<Events, Event>(db.Events.First(m => m.Name == "Jazz Fest"));
+
+                Assert.NotEqual(0, Event.Id); // should get some generated ID
+                Assert.Equal(EventSaved.Id, Event.Id);
+                Assert.Equal("Jazz Fest", Event.Name);
+                Assert.Equal("Music", Event.Type);
+                Assert.Equal(40, Event.Cost);
+            }
         }
 
         [Fact]
-        public override Task DeleteWithIdThatDoesntExistThrowsExceptionAsync()
+        public override async Task DeleteWithIdThatDoesntExistThrowsExceptionAsync()
         {
-            throw new NotImplementedException();
+            int id = 1000;
+
+            var options = new DbContextOptionsBuilder<Project15Context>()
+                .UseInMemoryDatabase("db_Event_test_delete").Options;
+
+            // arrange (use the context directl - we assume that it works)
+            using (var db = new Project15Context(options)) ;
+
+
+            // assert (for assert, once again use the context directly for verify.)
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+
+                Event Event = await repo.GetByIdAsync(id);
+
+                Assert.Null(Event);
+
+                await Assert.ThrowsAsync<ArgumentException>(() => repo.DeleteAsync(id));
+            }
         }
 
         [Fact]
-        public override Task DeleteWorksAsync()
+        public override async Task DeleteWorksAsync()
         {
-            throw new NotImplementedException();
+            Event EventSaved = null;
+            int id = 0;
+
+            // arrange (use the context directly - we assume that works)
+            var options = new DbContextOptionsBuilder<Project15Context>()
+                .UseInMemoryDatabase("db_Event_test_delete").Options;
+            using (var db = new Project15Context(options)) ;
+
+            // act (for act, only use the repo, to test it)
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+
+                //Create Event
+                Event Event = new Event { Name = "Jazz Fest", Type = "Music", Cost = 40 };
+                EventSaved = await repo.CreateAsync(Event);
+                await repo.SaveChangesAsync();
+
+                id = EventSaved.Id;
+            }
+
+            // assert (for assert, once again use the context directly for verify.)
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+                Event Event = await repo.GetByIdAsync(id);
+
+                Assert.NotEqual(0, Event.Id); // should get some generated ID
+                Assert.Equal(EventSaved.Id, Event.Id);
+                Assert.Equal("Jazz Fest", Event.Name);
+                Assert.Equal("Music", Event.Type);
+                Assert.Equal(40, Event.Cost);
+
+                await repo.DeleteAsync(id);
+                await repo.SaveChangesAsync();
+                Event = await repo.GetByIdAsync(id);
+
+                Assert.Null(Event);
+            }
         }
 
         [Fact]
-        public override Task GetAllWorksAsync()
+        public override async Task GetAllWorksAsync()
         {
-            throw new NotImplementedException();
+            List<Event> Events = new List<Event>();
+            Event EventSaved = null;
+
+            var options = new DbContextOptionsBuilder<Project15Context>()
+                .UseInMemoryDatabase("db_Event_test_getAll").Options;
+
+            using (var db = new Project15Context(options)) ;
+
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Event Event = new Event { Name = $"Jazz Fest {i}", Type = $"Music {i}", Cost = i };
+                    EventSaved = await repo.CreateAsync(Event);
+                    await repo.SaveChangesAsync();
+                    Events.Add(EventSaved);
+                }
+            }
+            using (var db = new Project15Context(options))
+            {
+
+                var repo = new EventRepository(db);
+                List<Event> EventList = (List<Event>)await repo.GetAllAsync();
+
+                // should equal the same amount of Events
+                Assert.Equal(Events.Count, EventList.Count);
+
+                for (int i = 0; i < Events.Count; i++)
+                {
+                    Assert.Equal(Events[i].Name, EventList[i].Name);
+                    Assert.Equal(Events[i].Type, EventList[i].Type);
+                    Assert.Equal(Events[i].Cost, EventList[i].Cost);
+                    Assert.NotEqual(0, Events[i].Id);
+                }
+            }
         }
 
         [Fact]
-        public override Task GetByIdThatDoesntExistReturnsNullAsync()
+        public override async Task GetByIdThatDoesntExistReturnsNullAsync()
         {
-            throw new NotImplementedException();
+            int id = 1000;
+
+            var options = new DbContextOptionsBuilder<Project15Context>()
+                          .UseInMemoryDatabase("db_Event_test_getAll").Options;
+
+            // arrange (use the context directly - we assume that works)
+            using (var db = new Project15Context(options)) ;
+
+            // assert (for assert, once again use the context directly for verify.)
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+
+                Event Event = await repo.GetByIdAsync(id);
+
+                Assert.Null(Event);
+            }
         }
 
         [Fact]
-        public override Task GetByIdWorksAsync()
+        public override async Task GetByIdWorksAsync()
         {
-            throw new NotImplementedException();
+            int id = 0;
+
+            Event EventSaved = null;
+
+            // arrange (use the context directly - we assume that works)
+            var options = new DbContextOptionsBuilder<Project15Context>()
+                .UseInMemoryDatabase("db_Event_test_getbyid").Options;
+            using (var db = new Project15Context(options)) ;
+
+            // act (for act, only use the repo, to test it)
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+                Event Event = new Event { Name = "Jazz Fest", Type = "Music", Cost = 40 };
+                EventSaved = await repo.CreateAsync(Event);
+                await repo.SaveChangesAsync();
+                id = EventSaved.Id;
+            }
+
+            // assert (for assert, once again use the context directly for verify.)
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+                Event Event = await repo.GetByIdAsync(id);
+
+                Assert.NotEqual(0, Event.Id); // should get some generated ID
+                Assert.Equal(EventSaved.Id, Event.Id);
+                Assert.Equal("Jazz Fest", Event.Name);
+                Assert.Equal("Music", Event.Type);
+                Assert.Equal(40, Event.Cost);
+            }
         }
 
         [Fact]
-        public override Task UpdateWithNoIdShouldReturnExceptionAsync()
+        public override async Task UpdateWithNoIdShouldReturnExceptionAsync()
         {
-            throw new NotImplementedException();
+            int id = 1000;
+
+            var options = new DbContextOptionsBuilder<Project15Context>()
+                          .UseInMemoryDatabase("db_Event_test_getAll").Options;
+
+            // arrange (use the context directly - we assume that works)
+            using (var db = new Project15Context(options)) ;
+
+            // assert (for assert, once again use the context directly for verify.)
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+
+                Event Event = await repo.GetByIdAsync(id);
+
+                Assert.Null(Event);
+
+                await Assert.ThrowsAsync<ArgumentException>(() => repo.UpdateAsync(Event, id));
+            }
         }
 
         [Fact]
-        public override Task UpdateWithWrongIdShouldReturnExceptionAsync()
+        public override async Task UpdateWithWrongIdShouldReturnExceptionAsync()
         {
-            throw new NotImplementedException();
+            int id = 0;
+            int wronId = 10;
+            Event EventSaved = null;
+            // arrange (use the context directly - we assume that works)
+            var options = new DbContextOptionsBuilder<Project15Context>()
+                .UseInMemoryDatabase("db_Event_test_update").Options;
+
+            // act (for act, only use the repo, to test it)
+            using (var db = new Project15Context(options)) ;
+
+            // assert (for assert, once again use the context directly for verify.)
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+
+                Event Event = new Event { Name = "Jazz Fest", Type = "Music", Cost = 40 };
+                EventSaved = await repo.CreateAsync(Event);
+                await repo.SaveChangesAsync();
+                id = EventSaved.Id;
+            }
+
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+
+                Event Event = await repo.GetByIdAsync(id);
+
+                Assert.NotEqual(0, Event.Id); // should get some generated ID
+                Assert.Equal(EventSaved.Id, Event.Id);
+                Assert.Equal("Jazz Fest", Event.Name);
+                Assert.Equal("Music", Event.Type);
+                Assert.Equal(40, Event.Cost);
+
+                await Assert.ThrowsAsync<ArgumentException>(() => repo.UpdateAsync(Event, wronId));
+
+            }
         }
 
         [Fact]
-        public override Task UpdateWorksAsync()
+        public override async Task UpdateWorksAsync()
         {
-            throw new NotImplementedException();
+            int id = 0;
+            int wrongId = 10;
+            Event EventSaved = null;
+            // arrange (use the context directly - we assume that works)
+            var options = new DbContextOptionsBuilder<Project15Context>()
+                .UseInMemoryDatabase("db_Event_test_update").Options;
+
+            // act (for act, only use the repo, to test it)
+            using (var db = new Project15Context(options)) ;
+
+            // act (for act, only use the repo, to test it)
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+
+                Event Event = new Event { Name = $"Jazz Fest", Type = $"Music", Cost = 40 };
+                EventSaved = await repo.CreateAsync(Event);
+                await repo.SaveChangesAsync();
+                id = EventSaved.Id;
+
+            }
+
+            // assert (for assert, once again use the context directly for verify.)
+            using (var db = new Project15Context(options))
+            {
+                var repo = new EventRepository(db);
+
+                Event Event = await repo.GetByIdAsync(id);
+
+                Assert.NotEqual(0, Event.Id); // should get some generated ID
+                Assert.Equal(EventSaved.Id, Event.Id);
+                Assert.Equal("Jazz Fest", Event.Name);
+                Assert.Equal("Music", Event.Type);
+                Assert.Equal(40, Event.Cost);
+
+
+                Event.Name = "Hip-Hop-Night";
+                Event.Type = "Music";
+
+                await repo.UpdateAsync(Event, id);
+                await repo.SaveChangesAsync();
+
+                Event = await repo.GetByIdAsync(Event.Id);
+
+                Assert.NotEqual(wrongId, Event.Id);
+                Assert.Equal("Hip-Hop-Night", Event.Name);
+                Assert.Equal("Music", Event.Type);
+            }
         }
     }
 }
