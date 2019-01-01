601aa0c7918d90275524043c9866b8857d90ff4e
Marty McFly <martinjnoyes12@gmail.com>
2019-01-01 16:28:05 -0600

    added ids, added browser log info to js file from working example

--- Project1-5_MVC_Consumer/Consumer/wwwroot/js/login.js
+++ Project1-5_MVC_Consumer/Consumer/wwwroot/js/login.js
@@ -3,5 +3,20 @@
 	event.preventDefault();
 
 	let role = $( this ).find( ':selected' ).data( 'role' );
-	alert( role );
+	sessionStorage.setItem( "role", role );
 } );
+
+//browser work to hide elements js worked jquery had issues
+//let x = document.getElementById( 'navHome' );
+//undefined
+//x
+//	< li id = "navHome" class="nav-item" >
+//		x.hidden;
+//false
+//x.hidden = true;
+//true
+//x.hide = false;
+//false
+//x.hidden = false;
+//false
+//$( 'navHome' ).hide();
